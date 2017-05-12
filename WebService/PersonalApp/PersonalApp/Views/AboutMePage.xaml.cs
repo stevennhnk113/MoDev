847529b3a1d2a0448f7280a55430d72628815103
Steven Nguyen <stevennhnk113@gmail.com>
2017-05-11 17:25:34 -0700

    added and finished WebService project

---
+++ WebService/PersonalApp/PersonalApp/Views/AboutMePage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PersonalApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AboutMePage : ContentPage
+	{
+		public AboutMePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
