46b983a60d19fce9a37bd3dec49c9cd7d6068b3c
Steven Nguyen <stevennhnk113@gmail.com>
2017-05-30 21:22:49 -0700

    added project for interview task

---
+++ InterviewTask/InterviewTask/InterviewTask/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace InterviewTask.ViewModels
+{
+	public class LoginViewModel : BaseViewModel
+	{
+		public LoginViewModel ()
+		{
+			Title = "Log In";
+		}
+	}
+}
