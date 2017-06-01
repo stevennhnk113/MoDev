9c66516ff849be1954d4f809deab70746e88ee07
Steven Nguyen <stevennhnk113@gmail.com>
2017-05-31 20:52:31 -0700

    added splash screen

--- InterviewTask/InterviewTask/InterviewTask.Android/MainActivity.cs
+++ InterviewTask/InterviewTask/InterviewTask.Android/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace InterviewTask.Droid
 {
-	[Activity(Label = "InterviewTask.Android", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity(Label = "InterviewTask.Android", Theme = "@style/MyTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate(Bundle bundle)
--- InterviewTask/InterviewTask/InterviewTask.Android/Resources/Resource.designer.cs
+++ InterviewTask/InterviewTask/InterviewTask.Android/Resources/Resource.designer.cs
@@ -2162,82 +2162,91 @@ namespace InterviewTask.Droid
 			public const int ic_tv_light = 2130837651;
 			
 			// aapt resource value: 0x7f020094
-			public const int mr_dialog_material_background_dark = 2130837652;
+			public const int LoginBackground = 2130837652;
 			
 			// aapt resource value: 0x7f020095
-			public const int mr_dialog_material_background_light = 2130837653;
+			public const int mr_dialog_material_background_dark = 2130837653;
 			
 			// aapt resource value: 0x7f020096
-			public const int mr_ic_audiotrack_light = 2130837654;
+			public const int mr_dialog_material_background_light = 2130837654;
 			
 			// aapt resource value: 0x7f020097
-			public const int mr_ic_cast_dark = 2130837655;
+			public const int mr_ic_audiotrack_light = 2130837655;
 			
 			// aapt resource value: 0x7f020098
-			public const int mr_ic_cast_light = 2130837656;
+			public const int mr_ic_cast_dark = 2130837656;
 			
 			// aapt resource value: 0x7f020099
-			public const int mr_ic_close_dark = 2130837657;
+			public const int mr_ic_cast_light = 2130837657;
 			
 			// aapt resource value: 0x7f02009a
-			public const int mr_ic_close_light = 2130837658;
+			public const int mr_ic_close_dark = 2130837658;
 			
 			// aapt resource value: 0x7f02009b
-			public const int mr_ic_media_route_connecting_mono_dark = 2130837659;
+			public const int mr_ic_close_light = 2130837659;
 			
 			// aapt resource value: 0x7f02009c
-			public const int mr_ic_media_route_connecting_mono_light = 2130837660;
+			public const int mr_ic_media_route_connecting_mono_dark = 2130837660;
 			
 			// aapt resource value: 0x7f02009d
-			public const int mr_ic_media_route_mono_dark = 2130837661;
+			public const int mr_ic_media_route_connecting_mono_light = 2130837661;
 			
 			// aapt resource value: 0x7f02009e
-			public const int mr_ic_media_route_mono_light = 2130837662;
+			public const int mr_ic_media_route_mono_dark = 2130837662;
 			
 			// aapt resource value: 0x7f02009f
-			public const int mr_ic_pause_dark = 2130837663;
+			public const int mr_ic_media_route_mono_light = 2130837663;
 			
 			// aapt resource value: 0x7f0200a0
-			public const int mr_ic_pause_light = 2130837664;
+			public const int mr_ic_pause_dark = 2130837664;
 			
 			// aapt resource value: 0x7f0200a1
-			public const int mr_ic_play_dark = 2130837665;
+			public const int mr_ic_pause_light = 2130837665;
 			
 			// aapt resource value: 0x7f0200a2
-			public const int mr_ic_play_light = 2130837666;
-			
-			// aapt resource value: 0x7f0200ad
-			public const int notification_template_icon_bg = 2130837677;
+			public const int mr_ic_play_dark = 2130837666;
 			
 			// aapt resource value: 0x7f0200a3
-			public const int profile_generic = 2130837667;
+			public const int mr_ic_play_light = 2130837667;
+			
+			// aapt resource value: 0x7f0200b0
+			public const int notification_template_icon_bg = 2130837680;
 			
 			// aapt resource value: 0x7f0200a4
-			public const int QQ = 2130837668;
+			public const int profile_generic = 2130837668;
 			
 			// aapt resource value: 0x7f0200a5
-			public const int RedBTN = 2130837669;
+			public const int QQ = 2130837669;
 			
 			// aapt resource value: 0x7f0200a6
-			public const int slideout = 2130837670;
+			public const int RedBTN = 2130837670;
 			
 			// aapt resource value: 0x7f0200a7
-			public const int Twitter = 2130837671;
+			public const int slideout = 2130837671;
 			
 			// aapt resource value: 0x7f0200a8
-			public const int Txt = 2130837672;
+			public const int SplashScreen = 2130837672;
 			
 			// aapt resource value: 0x7f0200a9
-			public const int TxtFoucus = 2130837673;
+			public const int SplashScreenLogo = 2130837673;
 			
 			// aapt resource value: 0x7f0200aa
-			public const int UnCheckBox = 2130837674;
+			public const int Twitter = 2130837674;
 			
 			// aapt resource value: 0x7f0200ab
-			public const int Wechat = 2130837675;
+			public const int Txt = 2130837675;
 			
 			// aapt resource value: 0x7f0200ac
-			public const int Weibo = 2130837676;
+			public const int TxtFoucus = 2130837676;
+			
+			// aapt resource value: 0x7f0200ad
+			public const int UnCheckBox = 2130837677;
+			
+			// aapt resource value: 0x7f0200ae
+			public const int Wechat = 2130837678;
+			
+			// aapt resource value: 0x7f0200af
+			public const int Weibo = 2130837679;
 			
 			static Drawable()
 			{
@@ -3115,8 +3124,8 @@ namespace InterviewTask.Droid
 			// aapt resource value: 0x7f0a015a
 			public const int Animation_Design_BottomSheetDialog = 2131362138;
 			
-			// aapt resource value: 0x7f0a0174
-			public const int AppCompatDialogStyle = 2131362164;
+			// aapt resource value: 0x7f0a0175
+			public const int AppCompatDialogStyle = 2131362165;
 			
 			// aapt resource value: 0x7f0a00a5
 			public const int Base_AlertDialog_AppCompat = 2131361957;
@@ -3564,6 +3573,9 @@ namespace InterviewTask.Droid
 			
 			// aapt resource value: 0x7f0a0173
 			public const int MyTheme_Base = 2131362163;
+			
+			// aapt resource value: 0x7f0a0174
+			public const int MyTheme_Splash = 2131362164;
 			
 			// aapt resource value: 0x7f0a0032
 			public const int Platform_AppCompat = 2131361842;
